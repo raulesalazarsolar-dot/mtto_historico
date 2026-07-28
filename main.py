6d869e3967f771ece69a7ab42a2118dfb532e10f
raulesalazarsolar-dot <raulesalazarsolar-dot@users.noreply.github.com>
2026-07-28 11:57:42 +0000

    deploy: f693524f151803bab265df0bf5689ebe8496b53d

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ SITE_URL = "https://teams.wal-mart.com/sites/EquipoPlanificacin"
 LIST_NAME = "Seguimiento Infraestructura"
 
 USERNAME = os.environ.get("SP_USERNAME", "r0r0noi@cl.wal-mart.com")
-PASSWORD = os.environ.get("SP_PASSWORD", "dEbit.spLiT+9")
+PASSWORD = os.environ.get("SP_PASSWORD", "Panic020")
 
 OUTPUT_HTML = "index.html"
 
